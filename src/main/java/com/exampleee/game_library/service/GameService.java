3fc7e02a518b3b0059d157709d4c098cf2b316e1
CelsiusBr <celsoaao7@gmail.com>
2026-03-30 19:36:48 -0300

    Feat: Service

---
+++ src/main/java/com/exampleee/game_library/service/GameService.java
@@ -0,0 +1,25 @@
+package com.exampleee.game_library.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exampleee.game_library.model.Game;
+import com.exampleee.game_library.repository.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository repository;
+
+    public Game save (Game jogo) {
+        return repository.save(jogo);
+    }
+
+    public List<Game> findAll() {
+        return repository.findAll();
+    }
+
+}
