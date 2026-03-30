cae06d79a7bf13d3257ff96b9c0bc46da8bb9c6b
CelsiusBr <celsoaao7@gmail.com>
2026-03-30 19:18:28 -0300

    Feat: Game Class

---
+++ src/main/java/com/exampleee/game_library/model/Game.java
@@ -0,0 +1,26 @@
+package com.exampleee.game_library.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+
+public class Game {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    private String plataforma;
+
+    private String genero;
+
+    private boolean zerado;
+}
