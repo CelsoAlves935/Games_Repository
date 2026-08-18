30620ccf0c0d982bc0e926125041d3ec3cb37883
Celso Alves <celsoaao7@gmail.com>
2026-08-18 18:22:48 -0300

    fix:BD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO game (titulo, plataforma, genero, zerado, platinado, nota_zerado, opiniao_zerado, nota_platina, opiniao_platina, url_capa) 
+VALUES ('The Witcher 3', 'PC', 'RPG', true, true, 10, 'Obra-prima!', 10, 'Platina trabalhosa mas gratificante', 'https://link-da-imagem.jpg');
+
+INSERT INTO game (titulo, plataforma, genero, zerado, platinado, nota_zerado, opiniao_zerado, nota_platina, opiniao_platina, url_capa) 
+VALUES ('Elden Ring', 'PlayStation 5', 'Soulslike', true, false, 9, 'Mundo incrível', null, null, 'https://link-da-imagem.jpg');
